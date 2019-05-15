df418ebd00e19f3019698670ae9b741f86257915
wonjaejeon92 <48269384+wonjaejeon92@users.noreply.github.com>
2019-05-15 16:03:12 +0900

    Update pfscripts.js

--- js/pfscripts.js
+++ js/pfscripts.js
@@ -1,3 +1,44 @@
-function scrolling(pos) {
-    $('html, body').animate({'scrollTop' : '$(pos).offset().top+px'}, 500);
+$(document).ready(function(){
+
+  $('.btn-1').click(function(){
+
+    var a = $('.skill');
+    var b = $('.work');
+    var c = $('.contact');
+    
+    if(a){
+      var offset = $(a).offset();
+    }
+    if(b){
+      var offset = $(b).offset();
+    }
+    if(c){
+      var offset = $(c).offset();
+    }
+    
+    
+    $('html,body').animate({scrollTop : offset.top }, 2000);
+
+  });
+
+});
+
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
 }
+
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0; 
+}
+
+
