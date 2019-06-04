4904a69690f41d313df5629d2570ff229409b4a9
wonjaejeon92 <48269384+wonjaejeon92@users.noreply.github.com>
2019-06-05 02:55:12 +0900

    Update pfscript.js

--- js/pfscript.js
+++ js/pfscript.js
@@ -33,7 +33,6 @@ $(document).ready(function(){
        getClassId = $('.contact');
        offset = getClassId.offset();
     }
-   console.log("offset : ",offset, "top : ",offset.top);
 
     $('html,body').animate({scrollTop : offset.top }, 2000);
  
@@ -60,4 +59,33 @@ function topFunction() {
   document.documentElement.scrollTop = 0; 
 }
 
+$(document).ready(function() {   
+  var sideslider = $('[data-toggle=collapse-side]');
+  var sel = sideslider.attr('data-target');
+  var sel2 = sideslider.attr('data-target-2');
+  sideslider.click(function(event){
+      $(sel).toggleClass('in');
+      $(sel2).toggleClass('out');
+  });
+});
 
+$(document).ready(function(){
+  var map;
+  function initialize(){
+    var myLatlng = new google.maps.LatLng(36.331483, 127.454790);
+    var mapOption = {
+      zoom:17,
+      center:myLatlng,
+      mapTypeId:google.maps.MapTypeId.ROADMAP
+    }
+    map = new google.maps.Map(document.getElementById('map-canvas'),mapOption);
+    google.maps.event.addListener(map,'zoom_changed',function(){ setTimeout(moveDaejeon, 3000);});
+    var market = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      title: "hi"
+    });
+    google.maps.addListener(marker,'click',function() {map.setZoom(10);});
+  }
+  google.maps.event.addDomListener(window,'load',initialize);
+})
